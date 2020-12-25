63db5000682e9658263509ce5f6a488e3437828e
Dmitriy Belkin <demetrius.belkin@gmail.com>
2020-12-25 02:23:33 +0200

    Выделен класс для работы с контактами

--- fixture/application.py
+++ fixture/application.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from fixture.session import SessionHelper
 from fixture.group import GroupHelper
+from fixture.contact import ContactHelper
 
 
 class Application:
@@ -10,38 +11,13 @@ class Application:
         self.wd.implicitly_wait(30)
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
+        self.contact = ContactHelper(self)
 
     # Contact
     def logout(self):
         # logout
         wd = self.wd
         wd.find_element_by_link_text("Logout").click()
-
-    def return_to_home_page(self):
-        # return to home page
-        wd = self.wd
-        wd.find_element_by_link_text("home").click()
-
-    def add_contact(self, contact):
-        # test add contact
-        wd = self.wd
-        wd.find_element_by_link_text("add new").click()
-        wd.find_element_by_name("firstname").click()
-        wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys(contact.firstname)
-        wd.find_element_by_name("lastname").click()
-        wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys(contact.lastname)
-        wd.find_element_by_name("address").click()
-        wd.find_element_by_name("address").clear()
-        wd.find_element_by_name("address").send_keys(contact.address)
-        wd.find_element_by_name("mobile").click()
-        wd.find_element_by_name("mobile").clear()
-        wd.find_element_by_name("mobile").send_keys(contact.mobile)
-        wd.find_element_by_name("email").click()
-        wd.find_element_by_name("email").clear()
-        wd.find_element_by_name("email").send_keys(contact.email)
-        wd.find_element_by_xpath("(//input[@name='submit'])[2]").click()
 
     def open_home_page(self):
         # open home page
--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -13,14 +13,14 @@ def app(request):
 
 def test_contact(app):
     app.session.login(username="admin", password="secret")
-    app.add_contact(Contact(firstname="Dmitriy", lastname="Belkin", address="Moscow", mobile="+79999999999", email="demetrius.belkin@gmail.com"))
-    app.return_to_home_page()
+    app.contact.add(Contact(firstname="Dmitriy", lastname="Belkin", address="Moscow", mobile="+79999999999", email="demetrius.belkin@gmail.com"))
+    app.contact.return_to_home_page()
     app.session.logout()
 
 
 def test_empty_contact(app):
     app.session.login(username="admin", password="secret")
-    app.add_contact(Contact(firstname="", lastname="", address="", mobile="", email=""))
-    app.return_to_home_page()
+    app.contact.add(Contact(firstname="", lastname="", address="", mobile="", email=""))
+    app.contact.return_to_home_page()
     app.session.logout()
 
