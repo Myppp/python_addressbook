dec20a2da7eb4089e56e9412e554b11a9957fa5f
Dmitriy Belkin <demetrius.belkin@gmail.com>
2020-12-25 03:30:25 +0200

    Сделана общая фикстура для всех тестов

--- fixture/contact.py
+++ fixture/contact.py
@@ -3,11 +3,6 @@ class ContactHelper:
     def __init__(self, app):
         self.app = app
 
-    def return_to_home_page(self):
-        # return to home page
-        wd = self.app.wd
-        wd.find_element_by_link_text("home").click()
-
     def add(self, contact):
         # test add contact
         wd = self.app.wd
@@ -28,3 +23,15 @@ class ContactHelper:
         wd.find_element_by_name("email").clear()
         wd.find_element_by_name("email").send_keys(contact.email)
         wd.find_element_by_xpath("(//input[@name='submit'])[2]").click()
+
+    def delete_first_contact(self):
+        wd = self.app.wd
+        self.app.open_home_page()
+        wd.find_element_by_name("selected[]").click()
+        wd.find_element_by_xpath("//input[@value='Delete']").click()
+        wd.switch_to_alert().accept()
+
+    def return_to_home_page(self):
+        # return to home page
+        wd = self.app.wd
+        wd.find_element_by_link_text("home").click()
