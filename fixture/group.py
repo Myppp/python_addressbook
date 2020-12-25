8e164b9d4a94cf3bc628a5b421e02f1c81a97eb9
Dmitriy Belkin <demetrius.belkin@gmail.com>
2020-12-25 02:52:06 +0200

    Сделана общая фикстура для всех тестов

--- fixture/group.py
+++ fixture/group.py
@@ -3,8 +3,8 @@ class GroupHelper:
     def __init__(self, app):
         self.app = app
 
-    def return_to_group_page(self):
-        # return to group page
+    def open_groups_page(self):
+        # open groups page
         wd = self.app.wd
         wd.find_element_by_link_text("groups").click()
 
@@ -27,7 +27,16 @@ class GroupHelper:
         wd.find_element_by_name("submit").click()
         self.return_to_group_page()
 
-    def open_groups_page(self):
-        # open groups page
+    def delete_first_group(self):
+        wd = self.app.wd
+        self.open_groups_page()
+        # select first group
+        wd.find_element_by_name("selected[]").click()
+        # submit deletion
+        wd.find_element_by_name("delete").click()
+        self.return_to_group_page()
+
+    def return_to_group_page(self):
+        # return to group page
         wd = self.app.wd
         wd.find_element_by_link_text("groups").click()
