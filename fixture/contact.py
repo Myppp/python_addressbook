ed4100f2eb39003c555372d5f12e38e1a5a141b5
Dmitriy Belkin <demetrius.belkin@gmail.com>
2021-01-07 20:10:44 +0200

    Добавил редактирование и удаление контакта и группы. Выполнил рефактор кода, исправил все ошибки.

--- fixture/contact.py
+++ fixture/contact.py
@@ -24,12 +24,40 @@ class ContactHelper:
         wd.find_element_by_name("email").send_keys(contact.email)
         wd.find_element_by_xpath("(//input[@name='submit'])[2]").click()
 
+    def edit_first_contact(self, contact):
+        wd = self.app.wd
+        self.app.open_home_page()
+        wd.find_element_by_xpath("//img[@alt='Edit']").click()
+        self.app.fill_form(contact)
+        wd.find_element_by_name("update").click()
+        self.app.return_home_page()
+
     def delete_first_contact(self):
         wd = self.app.wd
         self.app.open_home_page()
         wd.find_element_by_name("selected[]").click()
         wd.find_element_by_xpath("//input[@value='Delete']").click()
         wd.switch_to_alert().accept()
+
+    def modify_contact(self, contact):
+        wd = self.app.wd
+        wd.find_element_by_xpath("//img[@alt='Edit']").click()
+        wd.find_element_by_name("firstname").click()
+        wd.find_element_by_name("firstname").clear()
+        wd.find_element_by_name("firstname").send_keys(contact.firstname)
+        wd.find_element_by_name("lastname").click()
+        wd.find_element_by_name("lastname").clear()
+        wd.find_element_by_name("lastname").send_keys(contact.lastname)
+        wd.find_element_by_name("address").click()
+        wd.find_element_by_name("address").clear()
+        wd.find_element_by_name("address").send_keys(contact.address)
+        wd.find_element_by_name("mobile").click()
+        wd.find_element_by_name("mobile").clear()
+        wd.find_element_by_name("mobile").send_keys(contact.mobile)
+        wd.find_element_by_name("email").click()
+        wd.find_element_by_name("email").clear()
+        wd.find_element_by_name("email").send_keys(contact.email)
+        wd.find_element_by_xpath("(//input[@name='update'])[2]").click()
 
     def return_to_home_page(self):
         # return to home page
--- test/test_del_contact.py
+++ test/test_del_contact.py
@@ -1,4 +1,5 @@
-def test_delete_first_contact(app):
+def test_del_contact(app):
     app.session.login(username="admin", password="secret")
     app.contact.delete_first_contact()
+    app.contact.return_to_home_page()
     app.session.logout()
