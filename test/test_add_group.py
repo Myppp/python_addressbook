05e171c67b72c03af30773874e92b06df9902af1
Dmitriy Belkin <demetrius.belkin@gmail.com>
2021-01-26 13:45:00 +0200

    Задание №13: Доработать тесты так, чтобы в них удалялся или модифицировался случайно выбранный элемент

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -6,7 +6,7 @@ import string
 
 
 def random_string(prefix, maxlen):
-    symbols = string.ascii_letters + string.digits + string.punctuation + " "*10
+    symbols = string.ascii_letters + string.digits + string.punctuation + " " * 10
     return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
 
 
@@ -18,9 +18,9 @@ testdata = [Group(name="", header="", footer="")] + [
 
 @pytest.mark.parametrize("group", testdata, ids=[repr(x) for x in testdata])
 def test_add_group(app, group):
-        old_groups = app.group.get_group_list()
-        app.group.create(group)
-        assert len(old_groups) + 1 == app.group.count()
-        new_groups = app.group.get_group_list()
-        old_groups.append(group)
-        assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
+    old_groups = app.group.get_group_list()
+    app.group.create(group)
+    assert len(old_groups) + 1 == app.group.count()
+    new_groups = app.group.get_group_list()
+    old_groups.append(group)
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
